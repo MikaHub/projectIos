198f1b0741437084120e136dab4baa959706a5ed
Hugo Boutot <hugo.anthonyboutot@gmail.com>
2020-12-18 11:52:18 +0100

    new screen with 1 audio player

--- deezerApp/deezerApp/Controller/DetailViewController.swift
+++ deezerApp/deezerApp/Controller/DetailViewController.swift
@@ -6,24 +6,29 @@
 //
 
 import UIKit
+import AVFoundation
 
 class DetailViewController: UIViewController {
-
+    
+    var audioPlayer = AVAudioPlayer()
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        let sound = Bundle.main.path(forResource: "Solved", ofType: "mp3")
+        
+        do{
+            audioPlayer = try AVAudioPlayer(contentsOf: URL(fileURLWithPath: sound!))
+            try AVAudioSession.sharedInstance().setCategory(AVAudioSession.Category.playback, mode: AVAudioSession.Mode.default, options: [AVAudioSession.CategoryOptions.duckOthers])
+        } catch {
+            print(error)
+        }
         // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func playButtonPressed(_ sender: Any) {
+        audioPlayer.play()
     }
-    */
-
+    @IBAction func pauseButtonPressed(_ sender: Any) {
+        audioPlayer.pause()
+    }
 }
